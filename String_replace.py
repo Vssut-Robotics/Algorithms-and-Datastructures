35d010c3ec06c39e3af2acba095d435f2eb59029
Theminda Srimal <67902627+ThemindaSrimal@users.noreply.github.com>
2022-10-24 10:36:38 +0530

    Create a problem related to string replacing

---
+++ String_replace.py
@@ -0,0 +1,6 @@
+# Replace all instances of a substring in a string
+
+sentence = 'Sally sells sea shells by the sea shore'
+sentence.replace('sea', 'mountain')
+
+# => 'Sally sells mountain shells by the mountain shore'
